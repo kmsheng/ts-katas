8caca34d8ca2bb9680d19bae4b8619f0b2f2511b
kmsheng <kmsh3ng@gmail.com>
2018-03-12 08:36:01 +0800

    add interface example

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,7 +1,12 @@
-function greeter(person: String) {
-  return "Hello," + person;
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+  return "Hello," + person.firstName + " " + person.lastName;
+}
+
+let user = {firstName: "Jane", lastName: "Blah"};
 
 document.body.innerHTML = greeter(user);
